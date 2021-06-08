662750c40a914e85e9064b1f5098aaaa537c5222
Nima Rahnema <nwrahnema@gmail.com>
2021-06-08 16:06:19 -0400

    change --spin-duration to --spin-speed to normalize spinner speed

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,7 +6,7 @@ declare module 'csstype' {
   interface Properties {
     // Add a CSS Custom Property
     '--spin-index'?: number;
-    '--spin-duration'?: number;
+    '--spin-speed'?: number;
     '--num-options'?: number;
   }
 }
