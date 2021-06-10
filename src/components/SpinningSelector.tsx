c733b0a4e922fe3fc081a31a6f2685526869579f
Nima Rahnema <nwrahnema@gmail.com>
2021-06-10 00:15:19 -0400

    Move animation to use Web Animations API. Add spinner toggle on click.

--- src/components/SpinningSelector.tsx
+++ src/components/SpinningSelector.tsx
@@ -1,5 +1,6 @@
 import styles from "./SpinningSelector.module.scss";
 import useWebAnimations from "@wellyshen/use-web-animations";
+import { useState } from "react";
 
 type Props = {
   spinSpeed?: number;
@@ -7,14 +8,36 @@ type Props = {
 };
 
 const SpinningSelector = (props: Props) => {
+  const [isSpinning, setIsSpinning] = useState(true);
+
+  const { ref, getAnimation } = useWebAnimations<HTMLDivElement>({
+    keyframes: {
+      transform: ["rotateX(0)", "rotateX(-360deg)"],
+    },
+    animationOptions: {
+      duration: (1000 * props.options.length) / (props.spinSpeed || 10),
+      iterations: Infinity,
+    },
+  });
+
+  const toggleSpinning = () => {
+    getAnimation()?.updatePlaybackRate(isSpinning ? 0 : 1);
+    setIsSpinning(!isSpinning);
+  };
+
   return (
     <div
       className={styles.container}
       style={{ "--spin-speed": props.spinSpeed, "--num-options": props.options.length }}
     >
-      <div className={styles.optionsList}>
+      <div className={styles.optionsList} ref={ref}>
         {props.options.map((option, i) => (
-          <div key={i} className={styles.option} style={{ "--spin-index": i }}>
+          <div
+            key={i}
+            className={styles.option}
+            onMouseDown={toggleSpinning}
+            style={{ "--spin-index": i }}
+          >
             {option}
           </div>
         ))}
